227a4ee937864623c7a677fe8224c4cd175ddded
pierremarie.dhaussy@gmail.com <pierremarie.dhaussy@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-06-22 17:47:03 +0000

    Daily commit

--- trunk/map.js
+++ trunk/map.js
@@ -15,5 +15,5 @@ const MAP = [
 	[IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1],
 	[IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, null, null, null],
 	[IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, null, null, null],
-	[IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, null, null, null]
+	[IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, IMAGES.herbe1, null, null, IMAGES.rocher1]
 ];
