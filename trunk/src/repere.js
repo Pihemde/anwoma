4e56c8f9daf561c35f5da1863c0a2c224b94cc63
bruno.macherel@gmail.com <bruno.macherel@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-06-23 15:51:16 +0000

    Formule pour changement de repere

--- trunk/src/repere.js
+++ trunk/src/repere.js
@@ -1,4 +1,3 @@
-
 function toRealCoord(coord) {
 	var angle = Math.PI/4;
 
@@ -8,8 +7,14 @@ function toRealCoord(coord) {
 	
 	return [coord.elements[0][0] / Math.sqrt(2) * 58, coord.elements[1][0] / Math.sqrt(2) * 30];
 }
-function run() {
-	console.log(toRealCoord([0, 1]));
-}
 
+function fromRealCoord(coord) {
+	var angle = -Math.PI/4;
+
+	var transform = Matrix.Rotation(angle);
+	var coord = $M([[coord[0] / 58 * Math.sqrt(2)], [coord[1] / 30 * Math.sqrt(2)]]);
+	coord = transform.multiply(coord);
+	
+	return [Math.round(coord.elements[0][0]), Math.round(coord.elements[1][0])];
+}
 
