9d9711360c1eba6871064a70daace2c1192b54ce
bruno.macherel@gmail.com <bruno.macherel@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-06-24 14:12:33 +0000

    gestion du zoom et du déplacement de la carte

--- src/map.js
+++ src/map.js
@@ -25,8 +25,9 @@ var Map = function() {
 		this.width = width;
 		this.height = height;
 		this.tiles = [];
-		this.offsetX = 0;
-		this.offsetY = 0;
+		this.offsetX = 100;
+		this.offsetY = 50;
+		this.zoom = 1;
 		for(var y=0; y<this.height; y++) {
 			this.tiles[y] = [];
 			for(var x=0; x<this.width; x++) {
@@ -37,11 +38,19 @@ var Map = function() {
 				this.tiles[y][x] = new Tile(tileDescr);
 			}
 		}
+		var map = this;
+		canvas.addEventListener("mousewheel", function(e) {
+			if(e.shiftKey) {
+				if(e.wheelDelta>0) map.zoom = map.zoom * 1.5;
+				else map.zoom = map.zoom / 1.5;
+				e.stopPropagation();
+			}
+		});
 	};
 	Class.prototype.toRealCoord = function(coord) {
 		var angle = Math.PI/4;
 		// Centrage de la carte sur l'origine des axes
-		var x = coord[0] - this.width/2;
+		var x = coord[0] - this.width /2;
 		var y = coord[1] - this.height/2;
 		// Rotation pour orienter la carte dans le bon sens
 		// TODO: Prendre en comte la direction N/E/S/W
@@ -49,11 +58,11 @@ var Map = function() {
 		coord = $M([[x], [y]]);
 		coord = transform.multiply(coord);
 		// Etirement de la carte pour s'adapter à la taille des images 
-		x = coord.elements[0][0] / Math.sqrt(2) * SQUARE_WIDTH;
-		y = coord.elements[1][0] / Math.sqrt(2) * SQUARE_HEIGHT;
+		x = coord.elements[0][0] / Math.sqrt(2) * SQUARE_WIDTH  * this.zoom;
+		y = coord.elements[1][0] / Math.sqrt(2) * SQUARE_HEIGHT * this.zoom;
 		// Déplacement de la carte pour la centrer correctement
-		x = x + this.canvas.width/2;
-		y = y + this.canvas.height/2;
+		x = x + this.canvas.width /2 + this.offsetX;
+		y = y + this.canvas.height/2 + this.offsetY;
 		// Optim pour faire référence a des pixels entier
 		x = Math.round(x);
 		y = Math.round(y);
@@ -63,11 +72,11 @@ var Map = function() {
 	Class.prototype.fromRealCoord = function(coord) {
 		var angle = -Math.PI/4;
 		// Déplacement de la carte pour la centrer correctement
-		var x = coord[0] - this.canvas.width/2;
-		var y = coord[1] - this.canvas.height/2;
+		var x = coord[0] - this.offsetX - this.canvas.width/2;
+		var y = coord[1] - this.offsetY - this.canvas.height/2;
 		// Etirement de la ccarte pour s'adapter à la taille des images
-		x = x / SQUARE_WIDTH  * Math.sqrt(2);
-		y = y / SQUARE_HEIGHT * Math.sqrt(2);
+		x = x / SQUARE_WIDTH  * Math.sqrt(2) / this.zoom;
+		y = y / SQUARE_HEIGHT * Math.sqrt(2) / this.zoom;
 		// Rotation pour orienter la carte dans le bon sens
 		// TODO: Prendre en comte la direction N/E/S/W
 		var transform = Matrix.Rotation(angle);
@@ -85,6 +94,10 @@ var Map = function() {
 	Class.prototype.paint = function() {
 		paintLand(this);
 		paintGrid(this);
+/*
+		this.context.scale(this.zoom, this.zoom);
+		this.zoom = 1;
+*/
 	}
 	function paintLand(map) {
 		// Clear all the map
--- src/tiles.js
+++ src/tiles.js
@@ -52,12 +52,12 @@ var Tile = function() {
 		if(!image) return;
 		if(!map) return;
 		var coord = map.toRealCoord([x, y]);
-		x = coord[0] - 29;// FIXME Il faut sortir ça dans une méthode 
-		y = coord[1] + 15;// FIXME Il faut sortir ça dans une méthode 
+		x = coord[0] - SQUARE_WIDTH /2*map.zoom;// FIXME Il faut sortir ça dans une méthode 
+		y = coord[1] + SQUARE_HEIGHT/2;// FIXME Il faut sortir ça dans une méthode 
 
 		x += (SQUARE_WIDTH - image.object.width) * 0.5;
 		y -= image.object.height * 0.5 + (image.object.height - SQUARE_HEIGHT) * 0.5;
-		map.context.drawImage(image.object, x, y);
+		map.context.drawImage(image.object, x, y, image.object.width*map.zoom, image.object.height*map.zoom);
 	}
 
 	return Class;
