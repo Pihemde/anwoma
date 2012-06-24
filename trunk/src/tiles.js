4175b8df88a0cfb6fd0ef2819d1b79ac07a45a7c
bruno.macherel@gmail.com <bruno.macherel@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-06-24 12:28:27 +0000

    fonction de changement de repere lié a la carte

--- trunk/src/tiles.js
+++ trunk/src/tiles.js
@@ -43,21 +43,21 @@ var Tile = function() {
 		this.tileDescr = tileDescr;
 	}
 	
-	Class.prototype.paint = function(context, x, y) {
+	Class.prototype.paint = function(map, x, y) {
 		if(!this.tileDescr) return;
-		draw(this.tileDescr.ground, context, x, y);
-		draw(this.tileDescr.building, context, x, y);
+		draw(this.tileDescr.ground, map, x, y);
+		draw(this.tileDescr.building, map, x, y);
 	}
-	function draw(image, context, x, y) {
+	function draw(image, map, x, y) {
 		if(!image) return;
-		if(!context) return;
-		var coord = toRealCoord([x, y]);
+		if(!map) return;
+		var coord = map.toRealCoord([x, y]);
 		x = coord[0] - 29;// FIXME Il faut sortir ça dans une méthode 
 		y = coord[1] + 15;// FIXME Il faut sortir ça dans une méthode 
 
 		x += (SQUARE_WIDTH - image.object.width) * 0.5;
 		y -= image.object.height * 0.5 + (image.object.height - SQUARE_HEIGHT) * 0.5;
-		context.drawImage(image.object, x, y);
+		map.context.drawImage(image.object, x, y);
 	}
 
 	return Class;
