dc26a08f264d487f353b42c4ba82c76141ff3cf3
bruno.macherel@gmail.com <bruno.macherel@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-06-26 21:26:04 +0000

    déplacement de la carte + gestion des évènements

--- src/GraphicalContext.js
+++ src/GraphicalContext.js
@@ -16,15 +16,17 @@ var GraphicalContext = function() {
 		this.height = height;
 		this.angle = orientation*Math.PI/2 + Math.PI/4;
 		this.zoom = 1;
-		this.offsetX = 100;
-		this.offsetY = 50;
+		this.offset = {x: 0, y: 0};
 		this.totoEnSlip = 666;
 		this.mousePosition = {x : 0,y : 0};
 		this.selectedTile = undefined;
+		this.listeners = {};
 		var gcontext = this;
 		canvas.addEventListener('mousemove', function(event) {gcontext.onmousemove(event);}, false);
 		canvas.addEventListener('click', function(event) {gcontext.onclick(event);}, false);
 		canvas.addEventListener("mousewheel", function(event) {gcontext.onmousewheel(event);});
+		canvas.addEventListener("mouseup", function(event) {gcontext.onmouseup(event);});
+		canvas.addEventListener("mousedown", function(event) {gcontext.onmousedown(event);});
 	};
 	
 	/**
@@ -35,19 +37,45 @@ var GraphicalContext = function() {
 	};
 	
 	Class.prototype.drawImage = function(image, position, offset) {
+		var width = image.width * this.zoom;
+		var height = image.height * this.zoom;
+
 		var c = this.toPixels(position);
 		var x = c.x - SQUARE_WIDTH / 2 * this.zoom;
 		var y = c.y + SQUARE_HEIGHT / 2;
 
-		x += (SQUARE_WIDTH - image.width) * 0.5;
-		y -= image.height * 0.5 + (image.height - SQUARE_HEIGHT) * 0.5;
+		x += (SQUARE_WIDTH - image.width) * 0.5 * this.zoom;
+		y -= image.height * 0.5 * this.zoom + (image.height - SQUARE_HEIGHT) * 0.5 * this.zoom;
 		if(!!offset) {
-			x += offset.x;
-			y += offset.y;
+			x += offset.x * this.zoom;
+			y += offset.y * this.zoom;
 		}
-		this.context.drawImage(image, x, y);
+		this.context.drawImage(image, x, y, width, height);
 	};
+
+	Class.prototype.addEventListener = function(eventType, callback, object) {
+		var listeners = this.listeners[eventType];
+		if(!listeners) {
+			listeners = [];
+			this.listeners[eventType] = listeners;
+		}
+		listeners.push({callback: callback, object: object}); 
+	}
 	
+	Class.prototype.fireEvent = function(eventType, event) {
+		var listeners = this.listeners[eventType];
+		if(!!listeners) {
+			for(var i=0; i<listeners.length; i++) {
+				var listener = listeners[i];
+				if(listener.object) {
+					listener.callback.apply(listener.object, [event]);
+				} else {
+					listener.callback(event);
+				}
+			}
+		}
+	}
+
 	Class.prototype.toRealCoord = function(coord) {
 		// Centrage de la carte sur l'origine des axes
 		var x = coord[0] - this.width /2;
@@ -61,8 +89,8 @@ var GraphicalContext = function() {
 		x = coord.elements[0][0] / Math.sqrt(2) * SQUARE_WIDTH  * this.zoom;
 		y = coord.elements[1][0] / Math.sqrt(2) * SQUARE_HEIGHT * this.zoom;
 		// Déplacement de la carte pour la centrer correctement
-		x = x + this.canvas.width /2 + this.offsetX;
-		y = y + this.canvas.height/2 + this.offsetY;
+		x = x + this.canvas.width /2 + this.offset.x;
+		y = y + this.canvas.height/2 + this.offset.y;
 		// Optim pour faire référence a des pixels entier
 		x = Math.round(x);
 		y = Math.round(y);
@@ -71,8 +99,8 @@ var GraphicalContext = function() {
 
 	Class.prototype.fromRealCoord = function(coord) {
 		// Déplacement de la carte pour la centrer correctement
-		var x = coord[0] - this.offsetX - this.canvas.width/2;
-		var y = coord[1] - this.offsetY - this.canvas.height/2;
+		var x = coord[0] - this.offset.x - this.canvas.width/2;
+		var y = coord[1] - this.offset.y - this.canvas.height/2;
 		// Etirement de la ccarte pour s'adapter à la taille des images
 		x = x / SQUARE_WIDTH  * Math.sqrt(2) / this.zoom;
 		y = y / SQUARE_HEIGHT * Math.sqrt(2) / this.zoom;
@@ -102,21 +130,26 @@ var GraphicalContext = function() {
 	};
 
 	Class.prototype.onmousemove = function(event) {
+		var evt = event || window.event;
 		this.mousePosition.x = event.clientX - this.canvas.offsetLeft + window.pageXOffset;
 		this.mousePosition.y = event.clientY - this.canvas.offsetTop + window.pageYOffset;
-		//console.log(this.mousePosition.x, this.mousePosition.y);
-		//console.log(fromRealCoord([mousePosition.x, mousePosition.y]));
+		var c = this.fromRealCoord([this.mousePosition.x, this.mousePosition.y]);
+
+		this.fireEvent("move", {position: this.fromRealCoord([this.mousePosition.x, this.mousePosition.y])});
+
+		if(!!this.moveMap) {
+			this.offset.x += evt.clientX - this.moveMap.x;
+			this.offset.y += evt.clientY - this.moveMap.y;
+			this.moveMap = {x: evt.clientX, y: evt.clientY};
+		}
 	};
 
 	Class.prototype.onclick = function(event) {
 		this.mousePosition.x = event.clientX - this.canvas.offsetLeft + window.pageXOffset;
 		this.mousePosition.y = event.clientY - this.canvas.offsetTop + window.pageYOffset;
 		var c = this.fromRealCoord([this.mousePosition.x, this.mousePosition.y]);
-		
-		// 1er cas, l'utilisateur a selectionné l'outil de destruction 
-		
-		// 2ème cas, l'utilisateur a selectionné un objet dans la liste des terrains/batiments/... 
-		
+
+		this.fireEvent("click", {position: c});
 	};
 	
 	Class.prototype.onmousewheel = function(event) {
@@ -129,6 +162,15 @@ var GraphicalContext = function() {
 			event.stopPropagation();
 		}
 	};
+
+	Class.prototype.onmousedown = function(event) {
+		var evt = event || window.event;
+		this.moveMap = {x: evt.clientX, y: evt.clientY};
+	}
+
+	Class.prototype.onmouseup = function(event) {
+		this.moveMap = undefined;
+	}
 		
 	return Class;
 }();
