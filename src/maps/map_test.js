532dc73997891e3c92048f07c7d092574de5a1e6
pierremarie.dhaussy@gmail.com <pierremarie.dhaussy@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-07-01 17:42:06 +0000

    Testing map

--- src/maps/map_test.js
+++ src/maps/map_test.js
@@ -12,6 +12,15 @@ for(var i = 0 ; i < MAP_SIZE.width ; i++) {
 }
 
 
+function circleWithSigns(i, j, m) {
+	for(var n = 0 ; n < m ; n++) {
+		MAP.push({clazz: 'Sign', position: {i:i+n, j:j-1}, orientation: ORIENTATION.N}); // top
+		MAP.push({clazz: 'Sign', position: {i:i-1, j:j+n}, orientation: ORIENTATION.N}); // left
+		MAP.push({clazz: 'Sign', position: {i:i+m, j:j+n}, orientation: ORIENTATION.N}); // right
+		MAP.push({clazz: 'Sign', position: {i:i+n, j:j+m}, orientation: ORIENTATION.N}); // bottom
+	}
+}
+
 MAP.push({
 	clazz: 'Farm', 
 	position: {i:7, j:2},
@@ -38,33 +47,29 @@ MAP.push({
 //MAP.push({clazz: 'granary', position: {i:7, j:5}, good: {type: GOOD_TYPE.WHEAT, quantity: 4}});
 
 
-MAP.push({clazz: 'Sign', position: {i:10, j:9}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:11, j:9}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:12, j:9}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:9, j:10}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:9, j:11}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:9, j:12}, orientation: ORIENTATION.N});
 MAP.push({clazz: 'Granary', position: {i:10, j:10}, good: {type: GOOD_TYPE.WHEAT, quantity: 2}});
-MAP.push({clazz: 'Sign', position: {i:10, j:13}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:11, j:13}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:12, j:13}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:13, j:10}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:13, j:11}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:13, j:12}, orientation: ORIENTATION.N});
-
+circleWithSigns(10, 10, 3);
 
 MAP.push({
 	clazz: 'Farm', 
 	position: {i:20, j:8},
 	good: {type: GOOD_TYPE.MEAT, quantity: 1},
 });
-MAP.push({clazz: 'Sign', position: {i:20, j:11}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:21, j:11}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:22, j:11}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:23, j:8}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:23, j:9}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:23, j:10}, orientation: ORIENTATION.N});
+circleWithSigns(20, 8, 3);
 
+
+MAP.push({
+	clazz: 'Warehouse', 
+	position: {i:3, j:10},
+	goods: [
+	        {type: GOOD_TYPE.MEAT, quantity: 4},
+	        {type: GOOD_TYPE.WEAPON, quantity: 3},
+	        {type: GOOD_TYPE.OLIVE, quantity: 4},
+	        {type: GOOD_TYPE.IRON, quantity: 1},
+	        {type: GOOD_TYPE.VEGETABLE, quantity: 3}
+    ]
+});
+circleWithSigns(3, 10, 3);
 
 
 MAP.push({
@@ -78,25 +83,16 @@ MAP.push({
 	        {type: GOOD_TYPE.GRAPE, quantity: 3}
     ]
 });
+circleWithSigns(15, 15, 3);
+
 MAP.push({
 	clazz: 'Farm', 
 	position: {i:1, j:2},
 	good: {type: GOOD_TYPE.MEAT, quantity: 1},
 });
-MAP.push({clazz: 'Sign', position: {i:1, j:1}, orientation: ORIENTATION.N});
+circleWithSigns(1, 2, 3);
 
 
 
-MAP.push({clazz: 'Sign', position: {i:3, j:19}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:4, j:19}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:2, j:19}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:2, j:20}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:2, j:21}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:2, j:22}, orientation: ORIENTATION.N});
 MAP.push({clazz: 'Library', position: {i:3, j:20}});
-MAP.push({clazz: 'Sign', position: {i:3, j:22}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:4, j:22}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:5, j:19}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:5, j:20}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:5, j:21}, orientation: ORIENTATION.N});
-MAP.push({clazz: 'Sign', position: {i:5, j:22}, orientation: ORIENTATION.N});
+circleWithSigns(3, 20, 2);
