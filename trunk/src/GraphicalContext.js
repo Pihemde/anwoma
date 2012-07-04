db397998f0f462228b601d868d79ca9e22095bf5
bruno.macherel@gmail.com <bruno.macherel@gmail.com@491bebcd-6d7e-d05e-220c-a2d7c2d00f0a>
2012-07-04 21:22:51 +0000

    Histoire de ne pas afficher ce qui est en dehors du terrain

--- trunk/src/GraphicalContext.js
+++ trunk/src/GraphicalContext.js
@@ -103,6 +103,17 @@ var GraphicalContext = function() {
 		}
 		this.preRenderContext.drawImage(image, x, y, width, height);
 	};
+	Class.prototype.isVisible = function(position) {
+		var c = this.position2Coord({i: position.i, j: position.j});
+		if(c.x>0 && c.y>0 && c.x<canvas.width && c.y<canvas.height) return true;
+		c = this.position2Coord({i: position.i+1, j: position.j});
+		if(c.x>0 && c.y>0 && c.x<canvas.width && c.y<canvas.height) return true;
+		c = this.position2Coord({i: position.i+1, j: position.j+1});
+		if(c.x>0 && c.y>0 && c.x<canvas.width && c.y<canvas.height) return true;
+		c = this.position2Coord({i: position.i, j: position.j+1});
+		if(c.x>0 && c.y>0 && c.x<canvas.width && c.y<canvas.height) return true;
+		return false;
+	}
 
 	Class.prototype.addEventListener = function(eventType, callback, object) {
 		var listeners = this.listeners[eventType];
